dda0ae767b5f32a3b972d4d7d9f1f3a210d5f7fb
Julio de la Calle <juliodlcp@gmail.com>
2022-02-15 10:16:06 +0100

    Add devops

---
+++ challenges/devops/app/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+  "fmt"
+  "log"
+  "net/http"
+  "time"
+)
+
+func appHandler(w http.ResponseWriter, r *http.Request) {
+
+  fmt.Println(time.Now(), "Hello from my new fresh server")
+
+}
+
+func main() {
+  http.HandleFunc("/", appHandler)
+
+  log.Println("Started, serving on port 8080")
+  err := http.ListenAndServe(":8080", nil)
+
+  if err != nil {
+    log.Fatal(err.Error())
+  }
+}
